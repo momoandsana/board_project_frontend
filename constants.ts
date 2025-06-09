c4524f8c7e94fdafff9db579229a0265da127dee
Ho Seok <139951440+momoandsana@users.noreply.github.com>
2025-06-09 11:48:58 +0900

    Update constants.ts
    fix : render 주소 변경

--- constants.ts
+++ constants.ts
@@ -1,3 +1,3 @@
 // constants.ts
-export const API_BASE_URL = "https://your-render-service-name.onrender.com"; 
+export const API_BASE_URL = "https://board-project-backend.onrender.com"; 
 // 위 주소는 Render에서 실제 생성된 주소로 바꿔주세요.
